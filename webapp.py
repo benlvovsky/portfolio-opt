4134682d0e2bc0589790570c0c7e2cd8bd22443e
benlvovsky <ben@lvovsky.com>
2017-09-24 12:14:53 +1000

    upload file works

--- webapp.py
+++ webapp.py
@@ -1,6 +1,9 @@
 import markowitz as mark
 from flask import Flask
+from flask import request
 import json
+import settings as st
+import os
 
 app = Flask(__name__)
 
@@ -14,3 +17,17 @@ def efficientFrontier():
     parsed = json.loads(markJson)
     prettyJson = json.dumps(parsed, indent=4, sort_keys=True)
     return prettyJson
+
+@app.route('/upload', methods=['POST'])
+def uploadcsv():
+    f = request.files['the_file']
+    uploadDir = st.config["common"]["upload_directory"]
+    
+    if not os.path.exists(uploadDir):
+        os.makedirs(uploadDir)
+    f.save('{}/{}'.format(uploadDir, st.config["common"]["upload_file_name"]))
+    return '''
+    {
+    result: OK
+    }
+    '''
