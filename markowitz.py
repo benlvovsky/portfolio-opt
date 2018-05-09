36c6db9e383c5147d72bab20e543c0e505650627
Benjamin Lvovsky <Benjamin.Lvovsky@auspost.com.au>
2018-05-09 13:14:32 +1000

    tiingo download works

--- markowitz.py
+++ markowitz.py
@@ -40,7 +40,7 @@ globalTop100Str = 'DDD,MMM,WBAI,WUBA,EGHT,AHC,AIR,AAN,ABB,ABT,ABBV,ANF,GCH,ACP,J
 globalTop200Str = 'AAPL,GOOGL,GOOG,MSFT,AMZN,FB,INTC,CSCO,CMCSA,PEP,AMGN,NVDA,TXN,AVGO,GILD,QCOM,KHC,PYPL,PCLN,ADBE,NFLX,CHTR,CELG,COST,SBUX,WBA,BIIB,BIDU,FOX,AABA,MDLZ,QQQ,AMAT,AMOV,TSLA,TMUS,MU,ADP,CSX,CME,MAR,ATVI,FOXA,ISRG,ESRX,REGN,CTSH,EBAY,INTU,JD,NXPI,VRTX,MNST,EQIX,EA,ADI,ILMN,LRCX,ROST,LBTYA,LBTYB,AMTD,LBTYK,ALXN,FISV,PCAR,NTES,DLTR,TROW,AAL,PAYX,WDC,IBKR,XEL,ADSK,SIRI,MYL,DISH,CERN,NTRS,WDAY,HBANO,VCSH,ORLY,FITB,MCHP,CTRP,INCY,PFG,WLTW,SBAC,EXPE,VCIT,ALGN,SWKS,INFO,DVY,SYMC,PFF,CHKP,XLNX,CTAS,KLAC,WYNN,LBRDK,LBRDA,BMRN,HBAN,VRSK,FAST,XRAY,NTAP,MXIM,ULTA,VOD,MELI,IDXX,CA,CBOE,VIA,ETFC,CTXS,ALNY,LSXMK,TTWO,SNPS,NDAQ,ASML,ANSS,LKQ,JBHT,FANG,NCLH,IPGP,SIVB,CHRW,SPLK,STX,EMB,CDNS,HOLX,CINF,MBB,SHPG,TEAM,ACGL,EXPD,CSJ,HAS,SEIC,MRVL,HSIC,GRMN,SHY,SNI,AKAM,AMD,YNDX,CSGP,ODFL,CGNX,CDW,VRSN,IAC,QVCA,RYAAY,STLD,SCZ,LULU,TRMB,VXUS,QVCB,VIAB,IBB,ZION,CPRT,NWS,FLEX,CDK,ON,NWSA,DOX,TSCO,EXEL,IEP,OTEX,DISCB,NKTR,AGNCB,JKHY,ACWI,EWBC,CZR,MTCH,QRVO,ABMD,NDSN,ALKS,DISCA,IXUS,OLED,SSNC,FFIV,JAZZ,BLUE,SGEN,DISCK,Z,ZG,BNDX,CG,SHV,FWONK,GT,SINA,FTNT,GLPI,IEF,SBNY,CIU,AGNC,MIDD,MKTX,PPC,WB,UHAL,COHR,HDS,NBIX,FSLR,COMM,QGEN'
 asxTop20StrYahoo = 'CBA.AX,WBC.AX,BHP.AX,ANZ.AX,NAB.AX,CSL.AX,WES.AX,TLS.AX,WOW.AX,MQG.AX,RIO.AX,TCL.AX,WPL.AX,SCG.AX,WFD.AX,IAG.AX,AMP.AX,BXB.AX,QBE.AX'
 asxTop20StrGoogle = 'ASX:CBA,ASX:WBC,ASX:BHP,ASX:ANZ,ASX:NAB,ASX:CSL,ASX:WES,ASX:TLS,ASX:WOW,ASX:MQG,ASX:RIO,ASX:TCL,ASX:WPL,ASX:SCG,ASX:WFD,ASX:IAG,ASX:AMP,ASX:BXB,ASX:QBE'
-smallGlobalStr = 'FB,INTC,CSCO,CMCSA'
+smallGlobalStr = 'AAPL,GOOGL,GOOG'
 
 def main():
     # sharpeAndCml('upload', 0.03, "")
@@ -49,7 +49,7 @@ def main():
     print 'will run downloadInstruments'
     # downloadInstruments('yahoo', asxTop20Str, start, end, 'dataAllcolsTop200.csv')
     # downloadInstruments('morningstar', 'AAPL,GOOGL', 'Close', start, end, 'dataAllcolsTop200.csv')
-    downloadInstruments(globalTop200Str, start, end)
+    downloadInstruments(smallGlobalStr, start, end)
 
 
 def getEfficientFrontierPortfolios(port, evols):
@@ -148,29 +148,6 @@ def getListAsyncTasks():
     return '{{"response":{{"tasklist":"{}"}}}}'.format(csvList)
 
 
-def downloadInstruments_old_yahooNotSupportedAnyMoreByPandas(source, symbols, start_date, final_date, downloadFileName):
-    print 'symbols.split={}'.format(symbols.split(','))
-    print 'source={}, start_date={}, final_date={}, downloadFileName={}'.format(source, start_date, final_date,
-                                                                                downloadFileName)
-    allColumnsDf = web.DataReader(symbols.split(','), source, start_date, final_date)
-    print 'Columns list from DataReader: {}'.format(allColumnsDf.columns.values)
-    if 'Adj Close' in allColumnsDf:
-        dataDf = allColumnsDf['Adj Close']
-    else:
-        print 'Requered column doesn''t exist. Using the first column {}'.format(allColumnsDf.columns.values[1])
-        dataDf = allColumnsDf[1]
-    downloadDir = 'downloads'
-    if not os.path.exists(downloadDir):
-        os.makedirs(downloadDir)
-    # dataDf.to_csv(downloadDir + '/downloadedInstruments1.csv')
-    dataDf.index.names = ['date']
-    newDf = dataDf.sort_index()
-    # newDf.to_csv(downloadDir + '/downlWithEmpty.csv')
-    newDf.dropna(axis=1, inplace=True)
-    newDf.to_csv(downloadDir + '/' + downloadFileName)
-    print 'done'
-
-
 def downloadInstruments(symbols, start_date, final_date):
     millis = int(round(time.time() * 1000))
     downloadFileName = '{}.csv'.format(str(millis))
@@ -192,14 +169,27 @@ def downloadInstruments(symbols, start_date, final_date):
                                                                                 downloadFileName)
     session = requests.session()
     session.headers = requests.utils.default_headers()
-    session.headers['Accept'] = 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8'
+    session.headers['Accept'] = 'text/html,application/json,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8'
     session.headers['Accept-Encoding'] = 'gzip, deflate, br'
     session.headers['Accept-Language'] = 'en,ru-RU;q=0.9,ru;q=0.8,en-US;q=0.7'
     session.headers['Cache-Control'] = 'max-age=0'
     session.headers['Connection'] = 'keep-alive'
     session.headers['Upgrade-Insecure-Requests'] = '1'
     session.headers['User-Agent'] = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.119 Safari/537.36'
-    print 'using headers v.3'
+    session.headers['Content-Type'] = 'application/json'
+    session.headers['Authorization'] = 'Token {}'.format(access_key)
+    print 'using headers v.6'
+    # url = "https://api.tiingo.com/tiingo/daily/{}/prices?startDate=2012-1-1&endDate=2016-1-1&token={}".\
+    #     format(symbolsArray[0], access_key)
+    # print 'url={}'.format(url)
+    # # readJsonDf = pd.read_json("https://api.tiingo.com/tiingo/daily/{symbol}/prices?startDate=2012-1-1&endDate=2016-1-1&token={}", symbolsArray[0], access_key)
+    # # requestResponse = requests.get("https://api.tiingo.com/tiingo/daily/googl/prices?startDate=2012-1-1&endDate=2016-1-1", headers=session.headers)
+    # requestResponse = requests.get(url, headers=session.headers)
+    # readJsonDf = pd.read_json(requestResponse.json())
+    # print requestResponse.json()
+    # readJsonDf.to_csv(directory + '/readJsonDf.csv')
+    # return
+
     # allColumnsOrigDf = web.DataReader(symbolsArray, datasource, start_date, final_date, access_key=access_key,
     #                                   session=session, retry_count=10, pause=0.3)
     allColumnsOrigDf = TiingoExt.TiingoExt(symbolsArray, start_date, final_date, api_key=access_key,
@@ -212,65 +202,22 @@ def downloadInstruments(symbols, start_date, final_date):
     else:
         print 'Column {} doesn''t exist. Using [''Symbol'', ''Date'', ''Close'']'.format(priceColumn)
         dataDf = allColumnsNoIndexDf[['Symbol', 'Date', 'Close']]
+    # dataDf.to_csv(downloadDir + '/downloadedInstrumentsOnlyOnePriceColRaw.csv')
+    newDf = pivot(dataDf)
+
     if not os.path.exists(directory):
         os.makedirs(directory)
 
     # allColumnsNoIndexDf.to_csv(downloadDir + '/downloadedInstrumentsNoIndexAllCols.csv')
-    # dataDf.to_csv(downloadDir + '/downloadedInstrumentsOnlyOnePriceColRaw.csv')
-    newDf = pivot(dataDf)
+    earliest_date = start_date + dt.timedelta(days=7)
+    cols_to_exclude =  newDf.loc[:earliest_date].isnull().all()
+    cols_to_keep    = ~cols_to_exclude
+    print 'Earliest date to compare: {}'.format(str(earliest_date))
+    print 'Columns to keep: {}'.format(str(cols_to_keep))
+    print 'Columns excluded: {}'.format(str(cols_to_exclude))
+    newDf = newDf.loc[:, cols_to_keep]
     newDf.dropna(axis=1, inplace=True)
-    newDf.to_csv(directory + '/' + downloadFileName)
-    print 'download instruments done'
-    return directory + '/' + downloadFileName
 
-
-def downloadInstrumentsTiingo(symbols, start_date, final_date):
-    millis = int(round(time.time() * 1000))
-    downloadFileName = '{}.csv'.format(str(millis))
-
-    datasource  = st.config['downloader']['datasource']
-    symbolColumn= st.config['downloader']['symbolColumn']
-    dateColumn  = st.config['downloader']['dateColumn']
-    priceColumn = st.config['downloader']['priceColumn']
-    directory   = st.config['downloader']['directory']
-    access_key  = st.config['downloader']['access_key']
-
-    symbolsArray= symbols.split(',')
-    print 'datasource ={}'.format(datasource)
-    print 'symbolName ={}'.format(symbolColumn)
-    print 'dateColumn ={}'.format(dateColumn)
-    print 'priceColumn={}'.format(priceColumn)
-    print 'symbols.split={}'.format(symbolsArray)
-    print 'source={}, start_date={}, final_date={}, downloadFileName={}'.format(datasource, start_date, final_date,
-                                                                                downloadFileName)
-    session = requests.session()
-    session.headers = requests.utils.default_headers()
-    session.headers['Accept'] = 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8'
-    session.headers['Accept-Encoding'] = 'gzip, deflate, br'
-    session.headers['Accept-Language'] = 'en,ru-RU;q=0.9,ru;q=0.8,en-US;q=0.7'
-    session.headers['Cache-Control'] = 'max-age=0'
-    session.headers['Connection'] = 'keep-alive'
-    session.headers['Upgrade-Insecure-Requests'] = '1'
-    session.headers['User-Agent'] = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.119 Safari/537.36'
-    print 'using headers v.2'
-    # pd.read_json("https://api.tiingo.com/tiingo/daily/googl/prices?token=b52aff4cd1d604991702bf6bec5151924323ac4a")
-    allColumnsOrigDf = TiingoExt.TiingoExt(symbolsArray, start_date, final_date, api_key=access_key,
-                                           retry_count=20, pause=0.1, extheaders=session.headers).read()
-    allColumnsNoIndexDf = allColumnsOrigDf.reset_index()
-    print 'Columns list from DataReader: {}'.format(allColumnsNoIndexDf.columns.values)
-    print 'Index from DataReader: {}'.format(allColumnsNoIndexDf.index)
-    if priceColumn in allColumnsNoIndexDf:
-        dataDf = allColumnsNoIndexDf[[symbolColumn,dateColumn, priceColumn]]
-    else:
-        print 'Column {} doesn''t exist. Using [''Symbol'', ''Date'', ''Close'']'.format(priceColumn)
-        dataDf = allColumnsNoIndexDf[['Symbol', 'Date', 'Close']]
-    if not os.path.exists(directory):
-        os.makedirs(directory)
-
-    # allColumnsNoIndexDf.to_csv(downloadDir + '/downloadedInstrumentsNoIndexAllCols.csv')
-    # dataDf.to_csv(downloadDir + '/downloadedInstrumentsOnlyOnePriceColRaw.csv')
-    newDf = pivot(dataDf)
-    newDf.dropna(axis=1, inplace=True)
     newDf.to_csv(directory + '/' + downloadFileName)
     print 'download instruments done'
     return directory + '/' + downloadFileName
@@ -284,14 +231,15 @@ def pivot(dataDf):
     print 'symbols in data: {}'.format(symbolsArray)
     dfRet = pd.DataFrame()
     for i, sym in enumerate(symbolsArray):
-        print 'sym={}'.format(sym)
         newSymData = dataDf.loc[dataDf[symbolColumn] == sym][[dateColumn, priceColumn]]
+        earliest_date = pd.to_datetime(newSymData['date']).min()
+        print 'sym={}, earliest date={}'.format(sym, earliest_date)
         # dataDf.to_csv('downloads/newSymData.csv')
         newSymData.columns = [dateColumn, sym]
         if i < 1:
             dfRet = newSymData.copy()
         else:
-            dfRet = pd.merge(dfRet, newSymData, on=dateColumn)
+            dfRet = pd.merge(dfRet, newSymData, on=dateColumn, how='outer')
 
     reversed = dfRet.set_index(dateColumn)
     reversed.index.names = ['date']
