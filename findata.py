5e5eea44bfa4a33dfbcddeba87c28a73c917e1ac
Benjamin Lvovsky <Benjamin.Lvovsky@auspost.com.au>
2018-06-05 17:55:11 +1000

    pivot, still not working

--- findata.py
+++ findata.py
@@ -16,48 +16,54 @@ import requests
 import quandlreader as qr
 import marketdata as md
 import converter as cv
+import numpy as np
+
 
 class FinDownloader():
     """
     Historical daily data downloader
     """
+
     def __init__(self, source, retry_count=3, pause=0.1,
                  timeout=30):
-        self.source         = source
-        self.retry_count    = retry_count
-        self.pause          = pause
-        self.timeout        = timeout
-        self.datasource     = st.config['downloader']['datasource']
-        self.symbolColumn   = st.config['downloader']['symbolColumn']
-        self.dateColumn     = st.config['downloader']['dateColumn']
-        self.priceColumn    = st.config['downloader']['priceColumn']
-        self.directory      = st.config['downloader']['directory']
-        self.access_key     = st.config['downloader']['access_key']
-        self.inputfilename  = st.config['downloader']['inputfilename']
+        self.source = source
+        self.retry_count = retry_count
+        self.pause = pause
+        self.timeout = timeout
+        self.datasource = st.config['downloader']['datasource']
+        self.symbolColumn = st.config['downloader']['symbolColumn']
+        self.dateColumn = st.config['downloader']['dateColumn']
+        self.priceColumn = st.config['downloader']['priceColumn']
+        self.directory = st.config['downloader']['directory']
+        self.access_key = st.config['downloader']['access_key']
+        self.inputfilename = st.config['downloader']['inputfilename']
 
     def downloadInstruments(self, symbols, start_date, final_date):
 
         millis = int(round(time.time() * 1000))
         downloadFileName = '{}.csv'.format(str(millis))
 
-        symbolsArray= symbols.split(',')
+        symbolsArray = symbols.split(',')
         print 'datasource ={}'.format(self.datasource)
         print 'symbolName ={}'.format(self.symbolColumn)
         print 'dateColumn ={}'.format(self.dateColumn)
         print 'priceColumn={}'.format(self.priceColumn)
         print 'symbols.split={}'.format(symbolsArray)
-        print 'source={}, start_date={}, final_date={}, downloadFileName={}'.format(self.datasource, start_date, final_date,
+        print 'source={}, start_date={}, final_date={}, downloadFileName={}'.format(self.datasource, start_date,
+                                                                                    final_date,
                                                                                     downloadFileName)
 
         session = requests.session()
         session.headers = requests.utils.default_headers()
-        session.headers['Accept'] = 'text/html,application/json,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8'
+        session.headers[
+            'Accept'] = 'text/html,application/json,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8'
         session.headers['Accept-Encoding'] = 'gzip, deflate, br'
         session.headers['Accept-Language'] = 'en,ru-RU;q=0.9,ru;q=0.8,en-US;q=0.7'
         session.headers['Cache-Control'] = 'max-age=0'
         session.headers['Connection'] = 'keep-alive'
         session.headers['Upgrade-Insecure-Requests'] = '1'
-        session.headers['User-Agent'] = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.119 Safari/537.36'
+        session.headers[
+            'User-Agent'] = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.119 Safari/537.36'
         session.headers['Content-Type'] = 'application/json'
         session.headers['Authorization'] = 'Token {}'.format(self.access_key)
         print 'using headers v.6'
@@ -97,7 +103,7 @@ class FinDownloader():
             print 'Column {} doesn''t exist. Using [''Symbol'', ''Date'', ''Close'']'.format(self.priceColumn)
             dataDf = allColumnsNoIndexDf[['Symbol', 'Date', 'Close']]
         # dataDf.to_csv(downloadDir + '/downloadedInstrumentsOnlyOnePriceColRaw.csv')
-        newDf = self.pivot(dataDf)
+        newDf = self.pivot(dataDf, start_date, final_date)
 
         if not os.path.exists(self.directory):
             os.makedirs(self.directory)
@@ -108,40 +114,62 @@ class FinDownloader():
         existing_earliest_date = newDf.index.min()
         # existing_earliest_date = newDf.loc(newDf.index >= earliest_date).index.min()
         print 'earliest_date = {}, existing_earliest_date = {}'.format(earliest_date, existing_earliest_date)
-        cols_to_exclude =  newDf.loc[:existing_earliest_date].isnull().all()
-        cols_to_keep    = ~cols_to_exclude
+        cols_to_exclude = newDf.loc[:existing_earliest_date].isnull().all()
+        cols_to_keep = ~cols_to_exclude
         print 'Earliest date to compare: {}'.format(str(existing_earliest_date))
         print 'Columns to keep: {}'.format(str(cols_to_keep))
         print 'Columns excluded: {}'.format(str(cols_to_exclude))
         newDf = newDf.loc[:, cols_to_keep]
         newDf.to_csv(self.directory + '/before_dropna_' + downloadFileName)
         newDf.dropna(axis=0, inplace=True)
-        # newDf.dropna(axis=1, inplace=True)
+        newDf.dropna(axis=1, inplace=True)
         newDf.to_csv(self.directory + '/' + downloadFileName)
         print 'download instruments done'
         return self.directory + '/' + downloadFileName
 
-    def pivot(self, dataDf):
+
+    def pivot(self, dataDf, start_date, final_date):
         symbolsArray = dataDf[self.symbolColumn].unique()
-        print 'symbols in data: {}'.format(symbolsArray)
+        # print 'symbols in data: {}'.format(symbolsArray)
+        df_list=[]
         dfRet = pd.DataFrame()
         for i, sym in enumerate(symbolsArray):
             newSymData = dataDf.loc[dataDf[self.symbolColumn] == sym][[self.dateColumn, self.priceColumn]]
-            earliest_date = pd.to_datetime(newSymData['date']).min()
-            print 'sym={}, earliest date={}'.format(sym, earliest_date)
-            # dataDf.to_csv('downloads/newSymData.csv')
-            newSymData.columns = [self.dateColumn, sym]
-            if i < 1:
-                dfRet = newSymData.copy()
+            earliest_date = pd.to_datetime(newSymData[self.dateColumn]).min()
+            if start_date >= earliest_date:
+                print '{}.\tAdding sym=\'{}\': required {:%Y-%m-%d} >= earliest {:%Y-%m-%d}' \
+                    .format(i, sym, start_date, earliest_date)
+                # dataDf.to_csv('downloads/newSymData.csv')
+                newSymData[self.dateColumn] = pd.to_datetime(newSymData[self.dateColumn])
+                # newSymData[self.priceColumn] = newSymData[self.priceColumn].astype(np.float64)
+                newSymData = newSymData.set_index(self.dateColumn)
+                newSymData = newSymData[pd.to_datetime(newSymData.index) >= start_date]
+                newSymData.dropna(axis=(0, 1), inplace=True)
+                newSymData.drop_duplicates(inplace=True)
+                if newSymData.empty:
+                    print '\t\tSkipped as empty'
+                elif len(newSymData.index) < 200:
+                    print '\t\tSkipped as too short'
+                elif final_date - dt.timedelta(days=7) >= newSymData.index.max():
+                    print '\t\tSkipped as latest date {} is too far back'.format(newSymData.index.max())
+                else:
+                    newSymData.index.names = [self.dateColumn]
+                    newSymData.columns = [sym]
+                    newSymData.sort_index(inplace=True)
+                    newSymData.to_csv('downloads/{}.csv'.format(sym))
+                    df_list.append(newSymData)
             else:
-                dfRet = pd.merge(dfRet, newSymData, on=self.dateColumn, how='outer')
+                print '{}.\tSkipped. sym=\'{}\': required {:%Y-%m-%d} < earliest {:%Y-%m-%d}' \
+                    .format(i, sym, start_date, earliest_date)
 
-        dfRet[self.dateColumn] = pd.to_datetime(dfRet[self.dateColumn])
-        reversed = dfRet.set_index(self.dateColumn).sort_index()
-        # reversed.index = pd.to_datetime(reversed.index)
-        reversed.index.names = ['date']
-        reversed.to_csv('downloads/df_reversed.csv')
-        #  reversed = reversed.reindex(index=reversed.index[::-1])
-        data = reversed
-        data.columns = symbolsArray
-        return data
+        dfRet = pd.concat(df_list, axis=1, join='outer')
+        dfRet.to_csv('downloads/df_before_dropna_tresh.csv')
+        # dfRet.replace('', np.nan, inplace=True)
+        thresh = len(dfRet) * .2
+        dfRet = dfRet.dropna(thresh=thresh, axis=1)
+        dfRet.to_csv('downloads/df_after_dropna_tresh.csv')
+        dfRet.fillna(method='bfill', inplace=True)
+        dfRet.sort_index(inplace=True)
+        dfRet.index.names = ['date']
+        dfRet.to_csv('downloads/df_ret.csv')
+        return dfRet
